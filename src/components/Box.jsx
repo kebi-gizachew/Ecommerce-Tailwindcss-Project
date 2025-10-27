3d522da12f61c15029a2d1ab51db2f2b815a4cbe
Kebron Gizachew <kebisel21@gmail.com>
2025-10-27 06:03:07 +0300

    adding media query to me home and header components

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,18 +1,18 @@
 function Box(props) {
   return (
-    <div className="border-[2px] border-[rgba(239,239,239,1)] bg-white w-[16.5vw] h-[65vh] flex flex-col items-center p-[30px] pt-[30px]">
+    <div className="border-[2px] border-[rgba(239,239,239,1)] bg-white w-[16.5vw] h-[65vh] flex flex-col items-center p-[30px] pt-[30px] max-[700px]:w-[50%] max-[700px]:h-[20%]">
       <img src={props.image} className="rounded-[5%] w-[80%] h-[50%] mb-[20px] object-contain" />
       <div className="items-start flex flex-col">
-      <p className="text-[33px] text-left w-full mb-[50px] mt-[0px]">{props.text}</p>
+      <p className="text-[33px] max-[700px]:text-[25px] text-left w-full mb-[50px] mt-[0px]">{props.text}</p>
       
       <div className="mt-[0px] flex pl-0 gap-[20px] items-center">
-        <img src={props.rating} alt="Rating" className="w-[200px] h-[40px]"/>
-        <p className="text-[rgba(0,86,16,1)] text-[30px]">{props.aside}</p>
+        <img src={props.rating} alt="Rating" className="w-[200px] h-[40px] max-[700px]:w-[100px] max-[700px]:h-[30px] max-[700px]:mt-[0px]"/>
+        <p className="text-[rgba(0,86,16,1)] text-[30px] max-[700px]:text-[20px]">{props.aside}</p>
       </div>
       
-      <p className="my-[20px] text-[30px] font-bold">{props.cost}</p>
+      <p className="my-[20px] text-[30px] font-bold max-[700px]:text-[20px]">{props.cost}</p>
       
-      <select className="w-[25%] h-[60px] border-[2px] border-black rounded-[5px] text-3xl p-[10px] mb-[50px]">
+      <select className="w-[25%] h-[60px] border-[2px] border-black rounded-[5px] text-3xl p-[10px] mb-[50px] max-[700px]:h-[50px] max-[700px]:w-[50px] max-[700px]:text-[20px]">
         <option value="default">1</option>
         <option>2</option>
         <option>3</option>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,24 @@ import buyagain from '/images/icons/buy-again.png';
 import searchicon from '/images/icons/search-icon.png';
 import {NavLink} from 'react-router-dom';
 import products from '../data/products'
+import mobile from '/images/mobile-logo-white.png'
 function Header() {
   return (
-    <header className="w-full overflow-x-hidden h-[8vh] bg-[rgb(8,79,45)] text-white flex justify-between items-center">
-      <NavLink to="/" className="pointer-cursor"><img src={logowhite} alt="A white logo" className="ml-[50px] w-[340px] h-[43%]"/></NavLink>
+    <header className="w-full overflow-x-hidden h-[8vh] bg-[rgb(8,79,45)] text-white flex justify-between items-center max-[700px]:justify-around">
+      <NavLink to="/" className="pointer-cursor"><img src={mobile} className="hidden max-[700px]:flex max-[700px]:h-[40px]"/><img src={logowhite} alt="A white logo" className="ml-[50px] w-[340px] h-[43%] max-[700px]:w-[60%] max-[700px]:hidden"/></NavLink>
       
-      <div className="bg-white flex justify-between w-[55%] h-[65%] items-center rounded-[5px]">
-        <p className="text-[rgba(112,112,112,1)] text-4xl m-[35px]">Search</p>
-        <div className="w-[5%] h-full flex justify-center items-center text-[rgb(0, 0, 0)] bg-[rgb(186,255,190)] rounded-[0_5px_5px_0]">
+      <div className="bg-white flex justify-between w-[55%] h-[65%] items-center rounded-[5px] max-[700px]:w-[40%] max-[700px]:ml-[-40px]">
+        <p className="text-[rgba(112,112,112,1)] text-4xl m-[35px] max-[700px]:text-2xl">Search</p>
+        <div className="w-[5%] h-full flex justify-center items-center text-[rgb(0, 0, 0)] bg-[rgb(186,255,190)] max-[700px]:w-[10%] rounded-[0_5px_5px_0]">
           <img src={searchicon} alt="Search Icon" className="w-[50%]" />
         </div>
       </div>
       
-      <div className="w-[13%] flex justify-end items-center bg-transparent gap-[40px] mr-[40px]">
-        <NavLink to="/order" className="text-3xl font-bold">Orders</NavLink>
+      <div className="w-[13%] flex justify-end items-center bg-transparent gap-[40px] mr-[40px] max-[700px]:w-[10%] max-[700px]:mr-[10px]">
+        <NavLink to="/order" className="text-3xl font-bold max-[700px]:text-[20px]">Orders</NavLink>
         <div className="w-[40%] h-full flex justify-center items-center gap-[10px] ">
           <NavLink className="flex justify-center items-center gap-[10px]" to='/checkout'><img src={buyagain} alt="Buy Again Icon" className="w-[50%]" />
-          <p className="text-3xl font-bold">Cart</p>
+          <p className="text-3xl font-bold max-[700px]:text-[20px]">Cart</p>
           </NavLink>
         </div>
       </div>
