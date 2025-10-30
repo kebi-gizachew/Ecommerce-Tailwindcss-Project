3dba02c57e7b39e47790247e9e58dc288d65cf23
Kebron Gizachew <kebisel21@gmail.com>
2025-10-30 11:25:23 +0300

    doing the searching part in js

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,22 @@
 import Header from '../components/Header'
 import Box from '../components/Box'
 import products from '../data/products'
+import {useState} from 'react'
 function Home(){
-    const sets=products.map((items)=>(
-            {
-            id:items.id,
+    const [outputs,setOutputs]=useState(products)
+    const sets=(products!=outputs?outputs:products).map((items)=>({
+        id:items.id,
             img:items.image,
             para:items.name,
             rating:`/images/ratings/rating-${items.rating.stars*10}.png`,
             money:"$"+items.priceCents*0.01,
             aside:items.rating.count
-            }
-
+        }
     ))
+        
     return(
         <div className="font-robot">
-    <Header/>
+    <Header setOutputs={setOutputs}/>
     <div className="flex flex-wrap gap-[0px]">
     {sets.map((item)=>{
         return(
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,23 @@ import searchicon from '/images/icons/search-icon.png';
 import {NavLink} from 'react-router-dom';
 import products from '../data/products'
 import mobile from '/images/mobile-logo-white.png'
-function Header() {
+function Header({setOutputs}) {
+  const Filtering=(e)=>{
+      const value=e.target.value.toLowerCase().trim()
+      const filtered=products.filter(product=>{
+        const word= product.name.toLowerCase().split(' ')
+        return word.some(wor=>wor.includes(value))
+
+      }
+      )
+      setOutputs(filtered)
+  }
   return (
     <header className="w-full overflow-x-hidden h-[8vh] bg-[rgb(8,79,45)] text-white flex justify-between items-center max-[700px]:justify-around">
       <NavLink to="/" className="pointer-cursor"><img src={mobile} className="hidden max-[700px]:flex max-[700px]:h-[40px]"/><div><img src={logowhite} alt="A white logo" className="ml-[50px] w-[340px] h-[43%] max-[700px]:w-[60%] max-[700px]:hidden"/></div></NavLink>
       
       <div className="bg-white flex justify-between w-[55%] h-[65%] items-center rounded-[5px] max-[700px]:w-[40%] max-[700px]:ml-[-40px]">
-         <input className="w-[100%] h-full text-[rgba(112,112,112,1)] text-4xl p-[20px] max-[700px]:text-2xl" placeholder="Search"/>
+         <input className="w-[100%] h-full text-[rgba(112,112,112,1)] text-4xl p-[20px] max-[700px]:text-2xl" placeholder="Search" onChange={Filtering}/>
         <div className="w-[5%] h-full flex justify-center items-center text-[rgb(0, 0, 0)] bg-[rgb(186,255,190)] max-[700px]:w-[10%] rounded-[0_5px_5px_0]">
           <img src={searchicon} alt="Search Icon" className="w-[50%]" />
         </div>
